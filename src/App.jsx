533e016d2038138ddd1f23bc7c4d95d85eb18f80
ayobami7 <adeyemoayobami300@yahoo.com>
2024-04-08 12:13:57 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl">Hello World</h1>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
